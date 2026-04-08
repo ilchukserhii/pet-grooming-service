fe859c6010d36bdf2cb9eda6d2339e656e4e5674
Seroga <ilchuk.cergey@gmail.com>
2026-04-08 13:28:33 +0300

    add index page with templates, view and urls

--- grooming/views.py
+++ grooming/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from grooming.models import Service, Groomer, Pet
+
+
+def index(request):
+    our_services = Service.objects.all()
+    our_works = Pet.objects.filter(
+        appointments__is_completed=True
+    ).distinct()
+    context = {
+        "our_services": our_services,
+        "our_works": our_works,
+    }
+    return render(request, "grooming/index.html", context=context)
